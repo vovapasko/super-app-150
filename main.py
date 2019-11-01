a12986d8b3f5170d909283638119b9e4fd258d19
vovapasko <vovapasko1699@gmail.com>
2019-11-01 17:06:47 +0200

    Provided displaying all players, bets and banks data on the screen in flask

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import flask
 from flask import Flask, render_template
 
+from db import Database
+
 app = Flask(__name__)
 
+database = Database()
 
 @app.route('/')
 def hello():
@@ -16,17 +19,24 @@ def dashboard():
 
 @app.route('/players')
 def players():
-    return render_template("players.html")
+    all_players = database.fetchAllPlayers()
+    return render_template("players.html", all_players=all_players)
 
+
+@app.route('/players/<player_id>')
+def update_player(player_id):
+    return render_template()
 
 @app.route('/bets')
 def bets():
-    return render_template("bet.html")
+    all_bets = database.fetchAllBets()
+    return render_template("bet.html", all_bets=all_bets)
 
 
 @app.route('/banks')
 def banks():
-    return render_template("bank.html")
+    all_banks = database.fetchAllBanks()
+    return render_template("bank.html", all_banks=all_banks)
 
 
 if __name__ == '__main__':
