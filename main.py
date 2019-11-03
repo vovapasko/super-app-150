6dbc4fff5f40b36cd06cf47d73923fa1723c6c90
vovapasko <vovapasko1699@gmail.com>
2019-11-03 12:40:08 +0200

    Provided creating of new entity for bank, player and bet

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import flask
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from werkzeug.utils import redirect
+import os
 
 from db import Database
+from entities import Player, Bet, Bank
+from wtf_forms import PlayerForm, BetForm, BankForm
 
 app = Flask(__name__)
-
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 database = Database()
 
 
@@ -36,6 +40,22 @@ def delete_player(player_id):
     return redirect(url_for("players"))
 
 
+@app.route('/players/new_player', methods=["GET", "POST"])
+def create_player():
+    form = PlayerForm()
+    if request.method == "POST":
+        if not form.validate():
+            return render_template("create_player.html", form=form)
+        else:
+            id = form.id.data
+            balance = form.balance.data
+            passwrd = form.passwrd.data
+            player = Player(player_id=id, balance=balance, passwrd=passwrd)
+            database.createPlayer(player)
+            return redirect(url_for("players"))
+    return render_template("create_player.html", form=form)
+
+
 @app.route('/bets')
 def bets():
     all_bets = database.fetchAllBets()
@@ -53,6 +73,25 @@ def delete_bet(bet_id):
     return redirect(url_for("bets"))
 
 
+@app.route('/bets/new_bet', methods=["GET", "POST"])
+def create_bet():
+    form = BetForm()
+    if request.method == "POST":
+        if not form.validate():
+            return render_template("create_bet.html", form=form)
+        else:
+            id = form.bet_id.data
+            bet_money = form.bet_money.data
+            won_money = form.won_money.data
+            won_bet = form.won_bet.data
+            bet_time = form.bet_time.data.format('%Y-%m-%d %H:%M:%S')
+            bet = Bet(bet_id=id, bet_money=bet_money, won_money=won_money,
+                      won_bet=won_bet, bet_time=bet_time)
+            database.createBet(bet)
+            return redirect(url_for("bets"))
+    return render_template("create_bet.html", form=form)
+
+
 @app.route('/banks')
 def banks():
     all_banks = database.fetchAllBanks()
@@ -68,6 +107,22 @@ def update_bank(player_id, sold_time):
 def delete_bank(player_id, sold_time):
     database.deleteBank(player_id, sold_time)
     return redirect(url_for("banks"))
+
+
+@app.route('/banks/new_bank', methods=["GET", "POST"])
+def create_bank():
+    form = BankForm()
+    if request.method == "POST":
+        if not form.validate():
+            return render_template("create_bank.html", form=form)
+        else:
+            id = form.player_id.data
+            sold_time = form.sold_time.data.format('%Y-%m-%d %H:%M:%S')
+            sold_coins = form.sold_coins.data
+            bank = Bank(player_id=id, sold_coins=sold_coins, sold_time=sold_time)
+            database.createBank(bank)
+            return redirect(url_for("banks"))
+    return render_template("create_bank.html", form=form)
 
 
 if __name__ == '__main__':
---
+++ wtf_forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, SubmitField, FloatField, BooleanField
+from wtforms.validators import DataRequired
+
+
+class PlayerForm(FlaskForm):
+    id = IntegerField('Id', validators=[DataRequired()])
+    balance = IntegerField('Balance', validators=[DataRequired()])
+    passwrd = StringField('Password', validators=[DataRequired()])
+    Submit = SubmitField("Create")
+
+
+class BetForm(FlaskForm):
+    bet_id = IntegerField('Id', validators=[DataRequired()])
+    bet_money = FloatField('Money to bet', validators=[DataRequired()])
+    won_money = FloatField('Win money', validators=[DataRequired()])
+    won_bet = BooleanField('Bet won?', validators=[DataRequired()])
+    bet_time = StringField('Time of the bet', validators=[DataRequired()])
+    Submit = SubmitField("Create")
+
+
+class BankForm(FlaskForm):
+    player_id = IntegerField('player id', validators=[DataRequired()])
+    sold_time = StringField('Time of money selling', validators=[DataRequired()])
+    sold_coins = FloatField('Amount of coins', validators=[DataRequired()])
+    Submit = SubmitField("Create")
